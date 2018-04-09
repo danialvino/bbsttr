6e12e123c91ae5af8fe96e1df3b4d1a0dab860e3
Adriano Barroso <wiermann@broou.com>
2018-04-09 19:33:07 -0300

    include fullcalendar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,8 @@
+//= require jquery
 //= require rails-ujs
+//= require moment
+//= require fullcalendar
 //= require_tree .
+
+// document.getElementById('#calendar').fullCalendar({});
+$('#calendar').fullCalendar({});
