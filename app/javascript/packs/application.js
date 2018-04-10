aed61698aa057da8cf512057515164a0082b98ee
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-09 21:55:11 -0300

    include plugin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,26 @@
 import "bootstrap";
+import $ from 'jquery';
+import 'fullcalendar';
+import 'fullcalendar-scheduler';
+
+// document.getElementById('#calendar').fullCalendar({});
+
+$('#calendar').fullCalendar({
+    schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+
+    editable: true,
+    aspectRatio: 2,
+    scrollTime: '00:00', // undo default 6am scrollTime
+
+    header: {
+      left: 'today prev,next',
+      center: 'title',
+      right: 'agendaDay, agendaWeek,month,listWeek'
+    },
+    defaultView: 'agendaDay',
+    views: {
+    },
+    events: [
+      { id: '1', start: '2018-04-09T02:00:00', end: '2018-04-09T07:10:00', title: 'event 1' },
+    ]
+  });
