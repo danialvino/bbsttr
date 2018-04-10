bb87a586a46b18883670302956a4c258ebd71170
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-10 15:29:43 -0300

    Form for sitter and controller updates

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -9,4 +9,26 @@ class SittersController < ApplicationController
     @sitter = Sitter.find(params[:id])
   end
 
+  def new
+  @sitter = Sitter.new
+  end
+
+  def create
+    @sitter = Sitter.new(user_params)
+    @user= current_user
+    @sitter.user = @user
+    if @sitter.save
+      redirect_to root_path, notice: "Parabéns! agora você é uma Babá!"
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:sitter).permit(:pay_rate, :about, :skills, :user_id)
+  end
+
 end
--- app/models/sitter.rb
+++ app/models/sitter.rb
@@ -1,3 +1,5 @@
 class Sitter < ApplicationRecord
   belongs_to :user
+  serialize :skills, Array
+  validates :pay_rate, presence: true
 end
