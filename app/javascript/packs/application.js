dfa87d8b8cf5148fa74f4465fd73789145aa97cb
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-10 10:24:33 -0300

    calendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,20 +7,37 @@ import 'fullcalendar-scheduler';
 
 $('#calendar').fullCalendar({
     schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
-
-    editable: true,
-    aspectRatio: 2,
+    editable: false,
+    contentHeight: 455,
     scrollTime: '00:00', // undo default 6am scrollTime
-
+    firstDay: 0,
+    selectable: true,
+    // selectHelper: true, // makes it so you can make a placeholder event
     header: {
       left: 'today prev,next',
       center: 'title',
-      right: 'agendaDay, agendaWeek,month,listWeek'
+      right: 'agendaDay,agendaWeek,month,listWeek'
     },
     defaultView: 'agendaDay',
     views: {
+      agendaDay:{
+      allDaySlot: false,
+      nowIndicator: true,
+      },
+      agendaWeek:{
+      allDaySlot: false,
+      nowIndicator: true,
+
+      },
+      month:{
+        showNonCurrentDates: false,
+      },
+      listWeek:{
+      noEventsMessage: "No kids to take care.",
+      },
     },
     events: [
-      { id: '1', start: '2018-04-09T02:00:00', end: '2018-04-09T07:10:00', title: 'event 1' },
+      { id: '1', start: '2018-04-09T02:00:00', end: '2018-04-09T03:30:00', title: 'event 1' },
     ]
+
   });
