22df631c98d938bcca6b130cd94e7331c2d368e0
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-11 11:41:13 -0300

    Create seed, and add relation user many children

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "create Users"
+User.create!({
+  email: "pai@teste.com",
+  password: "huehue",
+  phone: 8199830,
+  birthday: Date.today,
+  name: "Painho",
+  gender: "Masculino",
+  address: "Rua ali a direita"
+  })
+User.create!({
+  email: "sitter@teste.com",
+  password: "huehue",
+  phone: 8199832,
+  birthday: Date.today,
+  name: "Mainha",
+  gender: "Feminino",
+  address: "Rua ali a esquerda"
+  })
+puts "ok!"
+puts "Create Sitter"
+Sitter.create!({
+  pay_rate: 69,
+  about: "Oi meu nome é Baba! esse é meu :about",
+  skills: ["Cozinhar"],
+  user_id: 2
+  })
+  BankInfo.create!({
+    agency: 02461,
+    account: 40362,
+    bank: "Banco do Brasil",
+    sitter_id: 1,
+    cpf: "050.290.934-00".to_i
+    })
+puts "ok!"
+puts "Creating a child (hehe)"
+Child.create!({
+  gender: "Masculino",
+  age: 5,
+  user_id: 1,
+  })
+puts "ok!"
