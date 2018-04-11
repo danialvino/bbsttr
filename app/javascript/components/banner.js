2d61c4f51398a359edb0021f82775ff0f948a20e
Daniel Topper <daniel.topper@gmail.com>
2018-04-11 15:45:08 -0300

    marginal changes

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["A melhor plataforma para achar a sua baby-sitter rapidamente", "A melhor plataforma para achar a sua baby-sitter facilmente", "A melhor plataforma para achar a sua baby-sitter com confiança"],
+    strings: ["blabla", "zrzr", "fsdf"],
     typeSpeed: 40,
     loop: true,
     showCursor: false,
