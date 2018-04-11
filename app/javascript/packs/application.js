73952355d6022035b8c8261d29bf757bb362b6fe
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-11 17:28:55 -0300

    fix banner type.js, calendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,45 +7,55 @@ import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
 // document.getElementById('#calendar').fullCalendar({});
 
-  // $('#calendar').fullCalendar({
-  //   schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
-  //   editable: false,
-  //   contentHeight: 455,
-  //   scrollTime: '06:00', // undo default 6am scrollTime
-  //   firstDay: 0,
-  //   selectable: true,
-  //   selectHelper: true, // makes it so you can make a placeholder event
-  //   header: {
-  //     left: 'today prev,next',
-  //     center: 'title',
-  //     right: 'agendaDay,agendaWeek,month,listWeek'
-  //   },
-  //   defaultView: 'listWeek',
-  //   views: {
-  //     agendaDay:{
-  //     allDaySlot: false,
-  //     nowIndicator: true,
-  //     },
-  //     agendaWeek:{
-  //     allDaySlot: false,
-  //     nowIndicator: true,
+var array_available = [];
+function createAvailableObject(element) {
+    // debugger
+  array_available.push(
+    {
+      title: "Oi",
+      start: element.dataset.start,
+      end: element.dataset.end
+    }
+  )
+}
 
-  //     },
-  //     month:{
-  //       showNonCurrentDates: false,
-  //     },
-  //     listWeek:{
-  //     noEventsMessage: "No kids to take care.",
-  //     },
-  //   },
-  //   events: [
-  //     // events: '/availables;'
-  //   // <% @availables.each do |available| %>
-  //   {
-  //     // title: "",
-  //     // start: '<%=available.start_time%>',
-  //     // end: '<%=available.start_time%>'
-  //   }
-  //   // <% end %>
-  //   ],
-  // });
+var elementsAvailable = $('.available');
+
+for (var i = 0; i < elementsAvailable.length; i++) {
+  createAvailableObject(elementsAvailable[i])
+}
+
+
+$('#calendar').fullCalendar({
+    schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+    editable: false,
+    contentHeight: 455,
+    scrollTime: '06:00', // undo default 6am scrollTime
+    firstDay: 0,
+    selectable: true,
+    selectHelper: true, // makes it so you can make a placeholder event
+    header: {
+      left: 'today prev,next',
+      center: 'title',
+      right: 'agendaDay,agendaWeek,month,listWeek'
+    },
+    defaultView: 'agendaWeek',
+    views: {
+      agendaDay:{
+      allDaySlot: false,
+      nowIndicator: true,
+      },
+      agendaWeek:{
+      allDaySlot: false,
+      nowIndicator: true,
+      },
+      month:{
+        showNonCurrentDates: false,
+      },
+      listWeek:{
+      noEventsMessage: "No kids to take care.",
+      },
+    },
+    resources: [],
+    events: array_available,
+  });
