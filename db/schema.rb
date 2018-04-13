8549e8963ee0d553d7fca65a5a5333f86d4c4d73
Danielle Alvino <daniellealvino@hotmail.com>
2018-04-13 15:49:06 -0300

    fixed schema sitter and created the form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413181537) do
+ActiveRecord::Schema.define(version: 20180413184302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,10 @@ ActiveRecord::Schema.define(version: 20180413181537) do
   end
 
   create_table "bank_infos", force: :cascade do |t|
-    t.integer "agency"
-    t.integer "account"
+    t.string "agency"
+    t.string "account"
     t.string "bank"
-    t.integer "cpf"
+    t.string "cpf"
     t.bigint "sitter_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
