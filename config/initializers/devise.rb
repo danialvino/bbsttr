1c7dfb9ef98183aa20e597dd1793148b3334a0be
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-15 21:18:25 -0300

    fix ouath2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
 
     config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
       scope: 'email',
-      info_fields: 'email, first_name, last_name',
+      info_fields: 'email',
       image_size: 'square',  # 50x50, guaranteed ratio
       secure_image_url: true
   # ==> Warden configuration
