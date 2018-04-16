f8fc69f8d3f8fd59fb83f0a642de330c81437a91
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-16 10:46:03 -0300

    fix facebook omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,20 +20,20 @@ class User < ApplicationRecord
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email)
+    user_params[:name] = auth.info.name
     user_params[:facebook_picture_url] = auth.info.image
     user_params[:token] = auth.credentials.token
     user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
     user_params = user_params.to_h
-
     user = User.find_by(provider: auth.provider, uid: auth.uid)
     user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
     if user
       user.update(user_params)
     else
-      # user = User.new(user_params)
-      # user.password = Devise.friendly_token[0,20]  # Fake password for validation
-      # user.save
-      redirect_to new_user_registration_path
+      user = User.new(user_params)
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.save
+      redirect_to new_user_registration_url
     end
 
     return user
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -258,12 +258,12 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-    config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
-      scope: 'email',
-      info_fields: 'email',
-      image_size: 'square',  # 50x50, guaranteed ratio
-      secure_image_url: true,
-      token_params: {token_params: {parse: :json}}
+  config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
+    scope: 'email',
+    info_fields: 'email',
+    image_size: 'square', # 50x50, guaranteed ratio
+    secure_image_url: true
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
