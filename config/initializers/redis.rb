8c22cdfcabce67eca4e145b53023e82565961690
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-17 11:15:54 -0300

    fixed bug

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -12,19 +12,3 @@ if url
   end
   $redis = Redis.new(:url => url)
 end
-UPDATE YOUR PROCFILE FILE
-# Procfile
-web: bundle exec puma -C config/puma.rb
-worker: bundle exec sidekiq -C config/sidekiq.yml
-Then you must:
-Commit
-Deploy
-Go to Heroku Dashboard to set dynos. Or your cli:
-heroku ps:scale worker=1
-heroku ps # Check worker dyno is running
-HEROKU CRON JOBS
-Use the Heroku Scheduler
-
-
-
-HAPPY ASYNCHRONIZING!
