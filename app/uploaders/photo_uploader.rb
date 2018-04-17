6abe5d8c87818f9b5af84049b882a0e91f774cc2
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-17 14:18:42 -0300

    cloudinary versions

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,20 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :profile do
+    cloudinary_transformation width: 200, height: 200, crop: :thumb, gravity: :face
+  end
+
+  version :avatar do
+    resize_to_fit 30, 30
+  end
+
+  version :bright_face do
+    cloudinary_transformation effect: "brightness:30", radius: 20,
+      width: 150, height: 150, crop: :thumb, gravity: :face
+  end
 end
